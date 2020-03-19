dafcad24b1c6101ad7202237804770487cf749e9
HenwiiZ <512849559@qq.com>
2020-03-18 23:48:23 -0400

    First Modified: Add comment

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 import sys
 
 def exist_matching(s1, s2):
+    """
+    Determine whether a one-to-one character mapping exists from one string, s1, to another string, s2.
+    :param s1: String 1
+    :param s2: String 2
+    :return: Whether the mapping exists(True or False)
+    """
     if (len(s1) != len(s2)):
         return False
 
     mapping = {}
+
+    #Check character of s1 and s2 one by one
     for i in range(len(s1)):
         if s1[i] not in mapping.keys():
             mapping[s1[i]] = s2[i]
